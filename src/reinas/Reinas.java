72a221a52a7c23171e19dc6ebb6ffce7db304b64
LaloGarcia1997 <lalog@DESKTOP-AUM7HA5>
2020-10-08 12:40:59 -0500

    Version 1

--- src/reinas/Reinas.java
+++ src/reinas/Reinas.java
@@ -5,64 +5,49 @@ import java.awt.*;
 
 
 class Reinas {
-     
-   // atributos
    private int fila;
    private int columna;
    private Reinas vecina;
-
-   // constructor
    Reinas (int c, Reinas v) {
-      fila = 1;   // empieza a buscar una solución desde la primera fila
-      columna = c; // la columna que se le ha asignado a esta reina
-      vecina = v;   // referencia a la reina de la izquierda
+      fila = 1;  
+      columna = c; 
+      vecina = v; 
    }
 
    public boolean buscaSolución() {
-      // si está bien colocada (nadie la puede atacar) ya es una solución:
-      if ( vecina == null || !vecina.puedeAtacar(fila, columna) ) return true;
-      
-      // si no, intenta avanzar a una posición donde no la puedan atacar:
-      if ( avanzar() ) return true;   
-      
-      return false; // no se ha encontrado una solución
+      if ( vecina == null || !vecina.puedeAtacar(fila, columna) ) 
+          return true;
+      if ( avanzar() ) 
+          return true;   
+      return false; 
    }
 
    public boolean avanzar() {   
-      if ( fila < 8 ) {   // avanzar una posición y mira si hay solución
+      if ( fila < 8 ) {  
          fila++;
          return buscaSolución();
       }
-
-      // si no puede avanzar, intenta que lo hagan las vecinas:
-      if ( vecina != null  && !vecina.avanzar() )      return false;
-      //if ( vecina == null  || !vecina.avanzar() )      return false;
-
-      // habiendo avanzado la vecina,
-      // vuelve a la primera fila para encontrar una nueva solución:
+      if ( vecina != null  && !vecina.avanzar() )     
+          return false;
       fila = 1;   
       return buscaSolución();   
    }
 
    private boolean puedeAtacar(int testfila, int testcolumna) {
       int diferenciaColumnas = testcolumna - columna;
-      if (   (fila == testfila) ||                  // en la misma fila
-            (fila + diferenciaColumnas == testfila) ||   // o en una diagonal
-            (fila - diferenciaColumnas == testfila))
-         return true;
-      
-      if (vecina != null)      // mira si es atacable por las demás reinas de la izda.
+      if (   (fila == testfila) || (fila + diferenciaColumnas == testfila) || (fila - diferenciaColumnas == testfila))
+         return true;      
+      if (vecina != null)      
          return vecina.puedeAtacar(testfila, testcolumna);
       
-      return false;           // la posición no es atacable
+      return false;         
    }
    
    void print() {
-      // primero lista las vecinas
-      if ( vecina!=null )   vecina.print();
-      // y luego su propia posición
+      if ( vecina!=null )  
+          vecina.print();
       System.out.println("columna "+columna+ " fila "+fila);
    }
   
-} // fin de class Reina
+}
 
--- src/reinas/asd.java
+++ src/reinas/asd.java
@@ -9,27 +9,17 @@ public class asd extends Applet {
    public void init() {
       últimaReina = null;
       for (int i = 1; i <= 8; i++) {
-         // crea una reina
          últimaReina = new Reinas(i, últimaReina);
-         // e intenta colocarla sin que nadie le ataque
          últimaReina.buscaSolución();
       }
    }
-
-   public boolean mouseDown(java.awt.Event evt, int x, int y) {
-      últimaReina.avanzar();  // busca otra solución
-      repaint();
-      return true;
-   }
    
-   public static void main(String[] args) { // hace lo mismo que el applet
-      int cuenta = 0; // cuenta de todas las posibles soluciones
+   public static void main(String[] args) { 
+      int cuenta = 0;
       Reinas ultimaReina = null;
       
       for (int i = 1; i <= 8; i++) {
-         // crea una reina
          ultimaReina = new Reinas(i, ultimaReina);
-         // e intenta colocarla sin que nadie le ataque
          if ( !ultimaReina.buscaSolución() )
                System.out.println("No hay solución");
       }   
